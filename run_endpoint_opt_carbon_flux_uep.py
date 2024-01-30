81cab501986e7e89ffc23020ec6e200158665dbe
Mike Stanley <mcstanle@andrew.cmu.edu>
2024-01-30 10:10:23 -0500

    updated code to restart ADMM at iteration 4 for UEP 11

--- run_endpoint_opt_carbon_flux_uep.py
+++ run_endpoint_opt_carbon_flux_uep.py
@@ -4,7 +4,7 @@ is called for UEP optimization.
 ===============================================================================
 Author        : Mike Stanley
 Created       : Jun 15, 2023
-Last Modified : Jan 22, 2024
+Last Modified : Jan 30, 2024
 ===============================================================================
 """
 from admm_optimizer import run_admm
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     MONTH_IDX = 9
     MU = 1e3  # penalty parameter enforcing feasibility
     READ_START_VECTORS = True  # read in previously saved w, c, and lambda vecs
-    START_IDX = 2  # should be 0 unless reading specific start vectors
+    START_IDX = 4  # should be 0 unless reading specific start vectors
 
     # define necessary directories
     HOME = '/glade/u/home/mcstanley'
@@ -157,9 +157,9 @@ if __name__ == "__main__":
 
     if READ_START_VECTORS:
         w_sp, c_sp, lambda_sp = read_starting_point(
-            w_fp=INT_START_DIR + '/w_start_from_it2.npy',
-            c_fp=INT_START_DIR + '/c_start_from_it2.npy',
-            lambda_fp=INT_START_DIR + '/lambda_start_from_it2.npy'
+            w_fp=INT_START_DIR + '/w_start_from_it4.npy',
+            c_fp=INT_START_DIR + '/c_start_from_it4.npy',
+            lambda_fp=INT_START_DIR + '/lambda_start_from_it4.npy'
         )
     else:
         w_sp, c_sp, lambda_sp = starting_point_generation(
@@ -201,5 +201,5 @@ if __name__ == "__main__":
     )
 
     # save the above output
-    with open(SAVE_DIR + '/final_results_from_it2.pkl', 'wb') as f:
+    with open(SAVE_DIR + '/final_results_from_it4.pkl', 'wb') as f:
         pickle.dump(res_dict, f)
